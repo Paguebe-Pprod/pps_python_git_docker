238d9c88319995e46d9d038ea804937afe70ac45
Pablo Guerrero <3960131@alu.murciaeduca.es>
2026-02-09 16:50:50 +0000

    Añadida aleatoriedad al Home de la aplicación

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, jsonify
 from bayeta import frotar
 
@@ -5,7 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>¡Hola, mundo! Bienvenido a La Bayeta de la Fortuna</h1>"
+    # Generamos un número aleatorio entre 1 y 10
+    repeticiones = random.randint(1, 10)
+    
+    # Creamos el texto base
+    frase = "¡Hola, mundo! Bienvenido a La Bayeta de la Fortuna.<br>"
+    
+    # Lo repetimos en bucle
+    contenido = ""
+    for i in range(repeticiones):
+        contenido += f"{i+1}. {frase}"
+    
+    return f"<html><body><h1>Sesión de hoy ({repeticiones} veces):</h1>{contenido}</body></html>"
 
 @app.route('/frotar/<int:n_frases>')
 def get_frases(n_frases):
@@ -13,5 +25,4 @@ def get_frases(n_frases):
     return jsonify({"frases": lista_frases, "cantidad": n_frases})
 
 if __name__ == '__main__':
-    # Ejecutamos en el puerto 5000
     app.run(host='0.0.0.0', port=5000)
